a4e5eab2325dd5ae3baad3951ced9872012c410f
Awa Melvine <melvineawa@gmail.com>
2019-07-12 10:48:35 +0100

    Integrate react router in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import ProjectContainer from "./components/projects/ProjectContainer";
 
 function App() {
   return (
     <div>
-      <ProjectContainer />
+      <Route exact path="/" component={ProjectContainer} />
+      <Route path="/project/:id" component={() => <div>test</div>} />
     </div>
   );
 }
--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Project = ({ project }) => {
   return (
     <div>
-      <h2>{project.name}</h2>
+      <Link to={`project/${project.id}`}>
+        <h2>{project.name}</h2>
+      </Link>
     </div>
   );
 };
