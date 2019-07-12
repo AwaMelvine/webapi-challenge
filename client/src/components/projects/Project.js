2e57fc47f6a789a9299607059fcfe9793fbe0b62
Awa Melvine <melvineawa@gmail.com>
2019-07-12 11:12:33 +0100

    Style single project component

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledProject = styled.div`
+  margin: 1rem auto;
+  background: white;
+  padding: 0.5rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+
+  h1 {
+    text-align: center;
+    color: #444;
+  }
+`;
+
+const StyledName = styled(Link)`
+  text-decoration: none;
+  font-family: "Courier New", Courier, monospace;
+`;
 
 const Project = ({ project }) => {
   return (
-    <div>
-      <Link to={`project/${project.id}`}>
+    <StyledProject>
+      <StyledName to={`project/${project.id}`}>
         <h2>{project.name}</h2>
-      </Link>
-    </div>
+      </StyledName>
+    </StyledProject>
   );
 };
 
